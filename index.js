637b0b915bafbd6bb541bcad817ac6566969c3bc
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-08-18 01:38:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+
+//Section 1
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+//Section 2
+const random = Math.ceil(Math.random() * 10) +1;
+
+//Section 3
+const num3 = 4;
+const num4 = 20;
+const mod = num3 % num4;
+
+//Section 4
+const max = (0,2,4,8,16,20);
+Math.max(Array.map(max));
